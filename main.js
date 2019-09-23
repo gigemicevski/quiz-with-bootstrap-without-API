9db95cf753e79fc7880d9fd9889befc5fff0700c
gigemicevski <gigemicevski@gmail.com>
2019-09-23 17:22:47 +0200

    check the answers

---
+++ main.js
@@ -0,0 +1,17 @@
+var correctAnswers = ["A","B","C","B"];
+var form = document.querySelector(".quiz-form");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,]
+
+    userAnswers.forEach((answer,index)=>{
+        if (answer===correctAnswers[index]){
+            score+=10;
+
+        }
+    });
+    console.log(score);
+})
